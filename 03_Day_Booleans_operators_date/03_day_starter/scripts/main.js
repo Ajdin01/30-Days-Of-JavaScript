18f22ef40495e8805f970e6255d39d64e9308fda
Ajdin01 <75125921+Ajdin01@users.noreply.github.com>
2022-04-07 01:13:20 +0200

    Day 2 functionally done
    Finished important bits, standard boolean stuff skipped

--- 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
+++ 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
@@ -1,1 +1,36 @@
 // this is your main.js script
+//let number = prompt('Enter number', 'number goes here')
+console.log(15)
+
+console.log(4>=3)
+/*let base = prompt('Enter base', 'base placeholder')
+let height = prompt('Enter height', 'height placeholder')
+
+alert(`The area of the triangle is ${0.5*base*height}`)*/
+
+let fName = 'Ajdin'
+let lName = 'Junuzovic'
+
+fName.length > lName.length
+    ? console.log(`Your first name ${fName} is longer than your last name ${lName}`)
+    : console.log('last name longer')
+
+
+const time = new Date()
+
+const year = time.getFullYear()
+const month = time.getMonth() + 1
+const day = time.getDate()
+
+const hours = time.getHours()
+const minutes = time.getMinutes()
+
+console.log(`${year}-${month}-${day} ${hours}:${minutes}`)
+console.log(`${day}-${month}-${year} ${hours}:${minutes}`)
+console.log(`${day}/${month}/${year} ${hours}:${minutes}`)
+
+
+console.log(`${day}/${month}/${year}`,
+    (hours <= 9 ? `0${hours}`: `${hours}`) +
+    (minutes <= 9 ? `:0${minutes}` : `:${minutes}`)
+)
